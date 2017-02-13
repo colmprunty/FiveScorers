fabef6053abd505a64337beb68aa0dfc1d59c56f
Colm Prunty <colmpruntysignups+git@gmail.com>
2017-02-13 21:11:06 +0000

    post player to DB

--- app_api/controllers/playerChoice.js
+++ app_api/controllers/playerChoice.js
@@ -5,8 +5,25 @@ module.exports.getPlayerChoices = function(req, res){
   Choice.find().
     exec(function(err, playerChoice){
       sendJsonResponse(res, 200, playerChoice);
-    })
+    });
 }
+
+var choosePlayer = function(req, res){
+
+}
+
+module.exports.choosePlayer = function(req, res){
+  Choice.create({
+    name: req.params.playerName
+  },
+  function(err, location){
+    if(err){
+      sendJsonResponse(res, 400, err);
+    } else {
+      sendJsonResponse(res, 201, location);
+    }
+  });
+};
 
 var sendJsonResponse = function(res, status, content){
   res.status(status);
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,4 +5,6 @@ var ctrlPlayerChoice = require('../controllers/playerChoice');
 
 router.get('/choices', ctrlPlayerChoice.getPlayerChoices);
 
+router.post('/playerChoice/:playerName', ctrlPlayerChoice.choosePlayer);
+
 module.exports = router;
