f09a1721d4b6ff4e3ca6e1f952f582e7b8da4f25
Colm Prunty <colmpruntysignups+git@gmail.com>
2017-03-20 22:25:02 +0000

    angular routing

--- app_client/app.js
+++ app_client/app.js
@@ -3,7 +3,7 @@ angular.module('FiveScorersApp', ['ngRoute']);
 function config($routeProvider){
   $routeProvider
     .when('/', {
-
+      templateUrl: '/home/home.view.html'
     })
     .otherwise( { redirectTo: '/'});
 }
