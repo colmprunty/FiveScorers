f6169193a1638ad66ff8ab03fc54f67d4c26abf2
Colm Prunty <colmpruntysignups+git@gmail.com>
2017-03-25 19:54:40 +0000

    angular routing works

--- app_client/app.js
+++ app_client/app.js
@@ -1,13 +1,8 @@
-angular.module('FiveScorersApp', ['ngRoute']);
-
-function config($routeProvider){
-  $routeProvider
-    .when('/', {
-      templateUrl: '/home/home.view.html'
-    })
-    .otherwise( { redirectTo: '/'});
-}
-
-angular
-  .module('FiveScorersApp')
-  .config(['$routeProvider', config]);
+angular.module("FiveScorersApp", ["ngRoute"])
+  .config(["$routeProvider", function($routeProvider){
+    $routeProvider
+      .when("/", {
+        templateUrl:"/home/home.view.html"
+      })
+      .otherwise({ redirectTo: "/"});
+  }]);
