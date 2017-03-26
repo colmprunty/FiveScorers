281ac2e80c10dd3a810e26c58d62ae818fbf8125
Colm Prunty <colmpruntysignups+git@gmail.com>
2017-03-26 17:30:42 +0100

    controller

--- app_client/app.js
+++ app_client/app.js
@@ -2,7 +2,9 @@ angular.module("FiveScorersApp", ["ngRoute"])
   .config(["$routeProvider", function($routeProvider){
     $routeProvider
       .when("/", {
-        templateUrl:"/home/home.view.html"
+        templateUrl:"/home/home.view.html",
+        controller: "homeCtrl",
+        controllerAs: "vm"
       })
       .otherwise({ redirectTo: "/"});
   }]);
---
+++ app_client/home/home.controller.js
@@ -0,0 +1,10 @@
+angular
+  .module('FiveScorersApp')
+  .controller('homeCtrl', homeCtrl);
+
+function homeCtrl (){
+  var vm = this;
+  vm.pageHeader = {
+    title: "Some number of scorers"
+  };
+}
