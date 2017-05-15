be606cc59c33d9d84434570d141740c50ba83e68
Colm Prunty <colmpruntysignups+git@gmail.com>
2017-05-15 19:40:33 +0100

    don't need these links either

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
+
 var appClientFiles = [
   'app_client/app.js',
   'app_client/home/home.controller.js'
