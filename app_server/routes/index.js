d11c9362cc3d35f1c7cfbb7fa65620721f1e0c75
Colm Prunty <colmpruntysignups+git@gmail.com>
2017-09-17 17:02:00 -0400

    removed app_server

--- app_server/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var ctrlPlayers = require('../controllers/players');
-var ctrlHome = require('../controllers/home')
-
-router.get('/', ctrlHome.angularApp);
-router.post('/')
-
-module.exports = router;
