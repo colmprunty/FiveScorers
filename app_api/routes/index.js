e9c5cfa975a0757f222137d362da47bcfda8f2f4
Colm Prunty <colmpruntysignups+git@gmail.com>
2018-01-15 22:07:25 +0000

    authentication stuff

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 var ctrlPlayerChoice = require('../controllers/playerChoice');
+var ctrlAuth = require('../controllers/authentication');
 
 router.get('/choices', ctrlPlayerChoice.getPlayerChoices);
 router.post('/playerChoice', ctrlPlayerChoice.choosePlayer);
+router.post('/register', ctrlAuth.register);
+router.post('/login', ctrlAuth.login);
 
 module.exports = router;
