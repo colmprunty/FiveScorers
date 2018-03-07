7d06a69af61a361c316728b94eb5243f3cb57ee5
Colm Prunty <colmpruntysignups+git@gmail.com>
2018-03-07 21:52:38 +0000

    select players only when authenticated

--- app_client/common/services/scorerData.service.js
+++ app_client/common/services/scorerData.service.js
@@ -3,13 +3,17 @@
         .module('FiveScorersApp')
         .service('scorerData', scorerData);
 
-    scorerData.$inject = ['$http'];
-    function scorerData($http){
+    scorerData.$inject = ['$http', 'authentication'];
+    function scorerData($http, authentication){
         var addChoice = function(playerName){
             var data = {
                 playerName: playerName
             };
-            return $http.post('/api/playerChoice', data);
+            return $http.post('/api/playerChoice', data, {
+                headers: {
+                    Authorization: 'Bearer ' + authentication.getToken()
+                }
+            });
         };
 
         var getChoices = function(){
